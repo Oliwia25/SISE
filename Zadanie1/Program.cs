a888e4e01a5acc786a6c9ac56889797a4f3c1a30
Oliwia <ofrontczak2547@gmail.com>
2020-03-16 19:59:36 +0100

    read file and write to file

--- Zadanie1/Program.cs
+++ Zadanie1/Program.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Zadanie1
 {
     class Program
     {
+        public static  List<string>  ReadFromFile(string filename)
+        {
+            List<string> allLinesText = File.ReadAllLines(filename).ToList();
+
+            return allLinesText;
+        }
+
+        public static void  WriteToFile(string filename, List<string> finishedBoard)
+        {
+            File.WriteAllLines(filename,finishedBoard);
+        }
+
         static void Main(string[] args)
         {
+            List<string> lines = ReadFromFile(@"C:\Users\Olivia\Desktop\gitHub\SISE\Zadanie1\ExampleBoard.TXT");
+            for(int i = 0; i < lines.Capacity; i++)
+            {
+                Console.WriteLine(lines);
+            }
+
+            WriteToFile(@"C:\Users\Olivia\Desktop\gitHub\SISE\Zadanie1\FinishedBoard.TXT",lines);
         }
     }
 }
