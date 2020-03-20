3742e7c586a8033e9001e30f0acf0d1d389066a4
Oliwia <ofrontczak2547@gmail.com>
2020-03-20 16:13:28 +0100

    Main

--- Zadanie1/Program.cs
+++ Zadanie1/Program.cs
@@ -16,7 +16,9 @@ namespace Zadanie1
 
         static void Main(string[] args)
         {
-            string lines = File.ReadAllText("ExampleBoard.TXT");
+            string inputFile;
+            inputFile = args[2];
+            string lines = File.ReadAllText(inputFile);
             string[] stringList = lines.Split((string[])null, StringSplitOptions.RemoveEmptyEntries);
             int[] intList = stringList.Select(arg => int.Parse(arg)).ToArray();
 
@@ -24,24 +26,32 @@ namespace Zadanie1
             initVert.PrintBoard();
             List<Vertex> solution = new List<Vertex>();
 
-            string option;
-            option = Console.ReadLine();
+            string option = args[0]; 
+            option = Console.ReadLine(); //to niepotrzebne chyba wtedy
+            string searchOrded;
+
+            
+            string solutionFile =  args[3];
+            string statisticFile = args[4];
 
             switch (option)
             {
                 case "bfs":                    
                     BFS bfs = new BFS();
                     solution = bfs.BfsSteps(initVert);
+                    searchOrded = args[1];
                     break;
                     
                 case "dfs":
                     DFS dfs = new DFS();
                     solution = dfs.DfsSteps(initVert);
+                    searchOrded = args[1];
                     break;
 
                 case "astar":
                     string heurestic;
-                    heurestic = Console.ReadLine();
+                    heurestic = Console.ReadLine(); //wywalić potem
+                    heurestic = args[1];
                     AStar aStar = new AStar(heurestic);
                     solution = aStar.AStarSteps(initVert);
                     break;
