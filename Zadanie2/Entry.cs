1f29798035eed5eb26f2c5cf35468a5483356ac9
Justyna <justyna.strawiak@gmail.com>
2020-05-07 21:32:37 +0200

    baby steps

---
+++ Zadanie2/Entry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie2
+{
+    class Entry
+    {
+        private double _input;
+        private double _weight;
+
+        public double Input { get => _input; set => _input = value; }
+        public double Weight { get => _weight; set => _weight = value; }
+    }
+}
---
+++ Zadanie2/Neuron.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie2
+{
+    class Neuron
+    {
+        private List<Entry> _entries;
+        private double _output;
+
+
+        public Neuron()
+        {
+            Entries = new List<Entry>();
+        }
+
+        public double Output { get => _output; set => _output = value; }
+        internal List<Entry> Entries { get => _entries; set => _entries = value; }
+
+        public double Sum()
+        {
+            double result = 0.0;
+            foreach(var e in Entries)
+            {
+                result += e.Input * e.Weight;
+            }
+            return result;
+        }
+
+
+
+    }
+}
